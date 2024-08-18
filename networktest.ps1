404d89884dae52006abebeee62707213d17b71a1
jasonphillips333 <70588683+jasonphillips333@users.noreply.github.com>
2024-08-18 23:43:26 +0100

    Add files via upload

---
+++ networktest.ps1
@@ -0,0 +1,30 @@
+﻿# ==================================================
+# System Network test Scan Program
+#
+# Author: Jason Phillips
+# Date Created: September 2024
+#
+# Description:
+#   [Network test can be used alone or part of splunk triggers]
+# ==================================================
+
+
+# Get network adapter information
+$adapters = Get-NetAdapter
+
+foreach ($adapter in $adapters) {
+    Write-Host "Adapter: $($adapter.Name)"
+    Write-Host "Link Speed: $($adapter.LinkSpeed)"
+}
+
+# Ping a website
+$pingResult = Test-Connection google.com -Count 4 -BufferSize 32768
+Write-Host "Average Ping: $($pingResult.ResponseTime.Average) ms"
+
+# Basic file transfer test (for demonstration purposes)
+$sourceFile = "C:\LargeFile.txt"
+$destinationFile = "D:\LargeFile.txt"
+$timeTaken = Measure-Command { Copy-Item $sourceFile $destinationFile }
+$fileSize = (Get-Item $sourceFile).Length
+$transferSpeed = $fileSize / $timeTaken.TotalSeconds
+Write-Host "Transfer Speed: $($transferSpeed) bytes/second"
---
+++ virusscan.ps1
@@ -0,0 +1,12 @@
+﻿# ==================================================
+# System Virus Scan Program
+#
+# Author: Jason Phillips
+# Date Created: September 2024
+#
+# Description:
+#   [Scan can be used alone or part of splunk triggers]
+# ==================================================
+# Run as administrator for full functionality
+Start-MpScan -ScanType FullScan 
+Start-MpScan -ScanType Custom -ScanPath C:\Users
